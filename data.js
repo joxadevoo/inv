32e1d00ab42d7106b81bbf2c1cab329247cbce4f
joxadevoo <joxadevoo@gmail.com>
2026-06-01 16:54:31 +0500

    Tizimning barcha ishlab chiqarish (production) kodlari va landing sahifasi qo'shildi

---
+++ data.js
@@ -0,0 +1,5 @@
+// Namunaviy boshlang'ich Tashkilot, Qavat va Xonalar ierarxiyasi (Locations Tree) - TOZALANDI
+const INITIAL_LOCATIONS = [];
+
+// Namunaviy boshlang'ich moddiy boyliklar ro'yxati (Aynan ierarxik joylashuvi bilan) - TOZALANDI
+const INITIAL_ASSETS = [];
